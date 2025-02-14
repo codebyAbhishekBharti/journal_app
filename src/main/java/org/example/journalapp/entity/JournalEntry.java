313441cc2be63eb4d3076ada87336b0fa1a81c0f
Abhishek Bharti <bhartiabhishek310@gmail.com>
2025-02-14 23:34:10 +0530

    Implemented lombok to prevent every time creating getter or setter for the project

--- src/main/java/org/example/journalapp/entity/JournalEntry.java
+++ src/main/java/org/example/journalapp/entity/JournalEntry.java
@@ -1,5 +1,8 @@
 package org.example.journalapp.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,33 +10,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Document(collection = "journalEntry")
+@Data
 public class JournalEntry {
     @Id
     private ObjectId id;
     private String content;
     private LocalDateTime date;
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
 }
