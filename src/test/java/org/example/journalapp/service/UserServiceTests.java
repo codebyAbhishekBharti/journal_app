90953d5f0f6c30927c5f5214aa0e76ae5252e8d9
Abhishek Bharti <bhartiabhishek310@gmail.com>
2025-02-26 00:20:45 +0530

    implementing Junit Testing

---
+++ src/test/java/org/example/journalapp/service/UserServiceTests.java
@@ -0,0 +1,14 @@
+package org.example.journalapp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class UserServiceTests {
+    @Test
+    public void testAdd(){
+        assertEquals(1, 1);
+    }
+}
