38b146da6ad86d9fa4901ef6450d91c4c2fed039
Abhishek Bharti <bhartiabhishek310@gmail.com>
2025-02-27 11:21:31 +0530

    learning looging through slf4j

--- src/main/java/org/example/journalapp/service/JournalEntryService.java
+++ src/main/java/org/example/journalapp/service/JournalEntryService.java
@@ -5,8 +5,6 @@ import org.example.journalapp.entity.JournalEntry;
 import org.example.journalapp.entity.User;
 import org.example.journalapp.repository.JournalEntryRepository;
 import org.example.journalapp.repository.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
